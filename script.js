120859ad3e2ea078640c086d17393276ee9ee186
vartika2000 <69324880+vartika2000@users.noreply.github.com>
2020-08-07 17:41:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "vartika"&&b=="jeenu")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
